d18bfd1821e5a75192e14ad67102f528014b214e
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-01-03 14:22:15 -0800

    rename user to skills

---
+++ models/skills.js
@@ -0,0 +1,13 @@
+const todos = [
+  {id: 125223, skill: 'JavaScipt', done: true},
+  {id: 127904, skill: 'linux', done: true},
+  {id: 139608, skill: 'Express', done: false}
+];
+
+module.exports = {
+  getAll
+};
+
+function getAll() {
+  return skills;
+}
