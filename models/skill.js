31308a2b8516c77ef583c572fd72559a1250fa99
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-01-03 15:43:08 -0800

    add css to skills

--- models/skill.js
+++ models/skill.js
@@ -1,6 +1,6 @@
 const skills = [
   {id: 125223, skill: 'JavaScipt', done: true},
-  {id: 127904, skill: 'linux', done: true},
+  {id: 127904, skill: 'HTML/CSS', done: true},
   {id: 139608, skill: 'Express', done: false}
 ];
 
