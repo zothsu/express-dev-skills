66f83d6928831dadb705aca58a3c436d7d5a9abe
Zoe Sullivan <0442338+zothsu@users.noreply.github.com.>
2024-01-04 15:56:44 -0800

    add titles to render

--- controllers/sillks.js
+++ controllers/sillks.js
@@ -10,18 +10,22 @@ module.exports = {
   };
 
 function newSkill(req, res) {
-  res.render('skills/new', {title: 'New Skill'})
+  res.render('skills/new', {
+    title: 'New Skill'
+  });
 }
 
 function index(req, res) {
     res.render('skills/index', {
-      skills: Skill.getAll()
+      skills: Skill.getAll(),
+      title:'Dev Skills'
     });
   }
 
   function show(req, res) {
     res.render('skills/show', {
       skill: Skill.getOne(req.params.id),
+      title: 'all skills'
     });
   }
   
